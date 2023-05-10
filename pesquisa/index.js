740d028624cb490dea3121c6b805ff1e2140436d
Alexffb <110848750+Alexffb32@users.noreply.github.com>
2023-05-10 21:51:09 +0100

    Update index.js

--- pesquisa/index.js
+++ pesquisa/index.js
@@ -1,33 +1,74 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('body,html').animate({
-      scrollTop: $(hash).offset().top
-      }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Modulo 10</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div id="loading">
+        <div id="spinner"></div>
+    </div>
+    <div id="header" class="animated slideInDown" style="animation-delay:1.8s;">
+    <div id="title">Modulo 10</div><br>
+    <div id="links">
+        <a href="#crud">CRUD</a>
+        <a href="#sql" style="margin:0px 60px;">SQL</a>
+    </div>
+    </div>
 
-var width = $(window).width(); 
+    <center>
+    <div id="middle">
+        <div id="tagline" class="animated zoomIn" style="animation-delay:1.8s;">
+            Entraste agora no modulo de SQL e ainda não sabes nada? Sem Problema<br>
+            <a href="#crud"><button class="btn_one"> Vamos estudar!</button></a>
+        </div>
+    </div>
+    </center>
 
-window.onscroll = function(){
-if ((width >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","100% auto");        
-    }
-}
-};
+    <div id="portfolio">
+             <div id="skills">
+                 <h1>CRUD</h1>   
+                 <div>
+                        <p>
+                            Na programação de computadores, Create (criar), Read (ler), Update (atualizar) e Delete (excluir), (CRUD) são as quatro funções básicas do armazenamento persistente. Às vezes, palavras alternativas são usadas ao definir as quatro funções básicas do CRUD, como recuperar em vez de ler, modificar em vez de atualizar ou destruir em vez de excluir. Às vezes, o CRUD também é usado para descrever as convenções da interface do usuário que facilitam a exibição, a pesquisa e a alteração de informações; geralmente usando formulários e relatórios baseados em computador. O termo provavelmente foi popularizado pela primeira vez por James Martin em seu livro de 1983, Gerenciando o ambiente de banco de dados. O acrônimo pode ser estendido para CRUDL para abranger a listagem de grandes conjuntos de dados que trazem complexidade adicional, como paginação quando os conjuntos de dados são muito grandes para manter facilmente na memória.”
+CRUD é um acrônimo que significa Criar, Ler, Atualizar e Excluir.
 
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1450);
+Estas são as quatro operações mais básicas que podem ser executadas com a maioria dos sistemas de banco de dados tradicionais e são a espinha dorsal para interagir com qualquer banco de dados.
+                              </p>
+                 </div> 
+             </div>
+         
+              <div id="sql">
+                 <h1>SQL</h1>
+
+                 <div class="project">
+                 <h2>project title 1</h2>
+                 <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum tincidunt nulla, id semper mi cursus ac. Fusce turpis urna, condimentum sit amet dolor at, vulputate ultrices massa. Vestibulum non euismod nulla, eu pretium risus. Curabitur porta molestie vulputate.</div>
+                 <button class="btn_one">visit project</button>
+                </div>
+
+                <div class="project">
+                 <h2>project title 2</h2>
+                 <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum tincidunt nulla, id semper mi cursus ac. Fusce turpis urna, condimentum sit amet dolor at, vulputate ultrices massa. Vestibulum non euismod nulla, eu pretium risus. Curabitur porta molestie vulputate.Lorem ipsum dolor sit amet, consectetur adipiscing elit. In bibendum tincidunt nulla, id semper mi cursus ac. Fusce turpis urna, condimentum sit amet dolor at, vulputate ultrices massa. Vestibulum non euismod nulla, eu pretium risus. Curabitur porta molestie vulputate.
+                 </div>
+                 <button class="btn_one">visit project</button>
+                 <button class="btn_two">code</button>
+                </div>
+
+             </div>
+         
+    </div>
+    <div id="footer">
+        made on earth by a human <br><br> <a href="https://imfunniee.github.io">imfunniee</a>
+    </div>
+<script src="index.js" type="text/javascript"></script>
+</body>
+</html>
